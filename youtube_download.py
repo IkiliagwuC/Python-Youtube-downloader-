819d5a0bfa234b7727e73de654e539d043afbccd
Ikiliagwu Chukwudi <57571996+IkiliagwuC@users.noreply.github.com>
2022-09-16 00:15:26 +0100

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -7,7 +7,7 @@ from pytube import YouTube
 download_folder = r"C:\Users\USER\Downloads"
 
 #remove hardcoding of the video url
-# video_url = "https://www.youtube.com/watch?v=5-INBjlqQWQ&ab_channel=FREESPEECH"
+# video_url = "https://www.youtube.com/watch?v=5-IN"
 
 video_obj = YouTube(video_url)
 
@@ -15,4 +15,4 @@ stream = video_obj.streams.get_highest_resolution()
 
 stream.download(download_folder)
 
-print("download successful, check folder for file")
+print("download successful, check download folder for file")
